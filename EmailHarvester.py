cca05240a788fcc597327deb3394838cd845c4f9
Jan Rude <whoot@users.noreply.github.com>
2018-09-04 10:54:04 +0200

    use correct path

--- EmailHarvester.py
+++ EmailHarvester.py
@@ -97,7 +97,7 @@ class EmailHarvester(object):
         self.userAgent = userAgent
         self.parser = myparser()
         self.activeEngine = "None"
-        path = os.path.join(os.path.dirname(sys.argv[0]) + "/plugins/")
+        path = os.path.dirname(os.path.abspath(__file__)) + "/plugins/"
         plugins = {}
         
         sys.path.insert(0, path)
